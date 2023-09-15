1bb7f9a8fd9638d7055d63fbca038e573bac1824
Liudmyla-By <132687246+Liudmyla-By@users.noreply.github.com>
2023-09-15 17:23:44 +0100

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div class="container">
-        <div class="weather-app">
+      <div className="container">
+        <div className="weather-app">
           <Search />
           <h1>Paris</h1>
           <ul>
