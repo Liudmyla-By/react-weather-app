0637d9bbe157325fcf10db0eb705b5584ab1a242
Liudmyla-By <132687246+Liudmyla-By@users.noreply.github.com>
2023-09-15 18:20:02 +0100

    Improved structure and CSS

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Search from "./Search.js"
 import Weather from "./Weather.js"
 import './App.css';
 
@@ -7,22 +6,12 @@ function App() {
   return (
     <div className="App">
       <div className="container">
-        <div className="weather-app">
-          <Search />
-          <h1>Paris</h1>
-          <ul>
-            <li>
-              Sunday 12:00
-            </li>
-            <li>Cloudy</li>
-          </ul>
-          <Weather humidity="70" wind="5" mainTemp="22" />
-        </div>
-        <footer>
-          This project was coded by Liudmyla Bykadorova and is
-          <a href="https://github.com/Liudmyla-By/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
-        </footer>
+        <Weather />
       </div>
+      <footer>
+        This project was coded by Liudmyla Bykadorova and is
+        <a href="https://github.com/Liudmyla-By/react-weather-app" target="_blank" rel="noreferrer"> open-sourced on GitHub</a>
+      </footer>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -1,29 +1,62 @@
-import "./App.css";
 import React from "react";
+import "./Weather.css";
 
-export default function DateDescription(props) {
-    return (
+export default function Weather() {
+  return (
+    <div className="Weather">
+      <form>
         <div className="row">
-            <div className="col-6">
-                <div className="flexbox weather-temperature">
-                    <img
-                        className="main-icon img-fluid"
-                        src="https://freesvg.org/img/weather-overcast.png"
-                        alt="weather-icon"
-                        id="icon"
-                    />
-                    <strong className="temperature" id="temperature">
-                        {props.mainTemp}
-                    </strong>
-                    <span className="unit">°C</span>
-                </div>
-            </div>
-            <div className="col-6 pe-0">
-                <ul className="humidity-wind">
-                    <li>Humidity: {props.humidity} %</li>
-                    <li>Wind: {props.wind} m/s</li>
-                </ul>
-            </div>
+          <div className="col-8">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Enter a city..."
+              id="search-input"
+              autoComplete="off"
+            />
+          </div>
+          <div className="col-2">
+            <input type="submit" className="btn btn-light" value="Search" />
+          </div>
+          <div className="col-2">
+            <input
+              type="submit"
+              className="btn d-none d-md-block btn-light"
+              value="Current"
+              id="button"
+            />
+          </div>
         </div>
-    );
+      </form>
+      <h1>Paris</h1>
+      <ul>
+        <li>
+          Sunday 12:00
+        </li>
+        <li>Cloudy</li>
+      </ul>
+      <div className="row">
+        <div className="col-6">
+          <div className="flexbox weather-temperature">
+            <img
+              className="main-icon img-fluid"
+              src="https://freesvg.org/img/weather-overcast.png"
+              alt="weather-icon"
+              id="icon"
+            />
+            <strong className="temperature" id="temperature">
+              22
+            </strong>
+            <span className="unit">°C</span>
+          </div>
+        </div>
+        <div className="col-6 pe-0">
+          <ul className="humidity-wind">
+            <li>Humidity: 55 %</li>
+            <li>Wind: 5 m/s</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
